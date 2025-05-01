4ff594245fd4aa82c0b741713beae070342dc52d
Ilham Syahid S <ilhamsyahids@gmail.com>
2025-05-01 15:39:45 +0700

    feat: implement WaPublisher for sending notifications via WhatsApp
    Signed-off-by: Ilham Syahid S <ilhamsyahids@gmail.com>

--- internal/core/models.go
+++ internal/core/models.go
@@ -1,5 +1,14 @@
 package core
 
 type Message struct {
-	ID string `json:"id"`
+	ID                 string   `json:"id"`
+	Content            string   `json:"content"`
+	RecipientNumbers   []string `json:"recipient_numbers"`
+	ScheduledSendingAt int64    `json:"scheduled_sending_at"`
+	RetriedCount       int      `json:"retried_count"`
+	Status             string   `json:"status"`
+	SentAt             *string  `json:"sent_at"`
+	Reason             *string  `json:"reason"`
+	CreatedAt          int64    `json:"created_at"`
+	UpdatedAt          int64    `json:"updated_at"`
 }
